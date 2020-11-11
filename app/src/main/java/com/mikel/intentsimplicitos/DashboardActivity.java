3a92f843b973a9a870540eaabd0111e9597a9f1e
Imanol Montoro <imanolma90@gmail.com>
2020-11-11 21:58:22 +0100

    Iconos de info y funcionalidad añadida

--- app/src/main/java/com/mikel/intentsimplicitos/DashboardActivity.java
+++ app/src/main/java/com/mikel/intentsimplicitos/DashboardActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,8 +24,10 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
     private Button nextButton = null;
     private ArrayList<String> things = null;
     private ImageView imageView = null;
+    private ImageButton imageButton=null;
     private Integer currentImagePosition = 0;
-private TextView objectTitle = null;
+    private TextView objectTitle = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +36,7 @@ private TextView objectTitle = null;
         this.nextButton = (Button) findViewById(R.id.nextButton);
         this.imageView = (ImageView) findViewById(R.id.imageView);
         this.objectTitle = (TextView) findViewById(R.id.objectTitle);
+        this.imageButton = (ImageButton) findViewById( R.id.imgbtn_Info );
 
         String concatenado = intent.getExtras().getString("user");
 
@@ -41,6 +49,57 @@ private TextView objectTitle = null;
                 nextObject();
             }
         });
+
+        imageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                goInfoWeb();
+            }
+        });
+    }
+
+    private void goInfoWeb() {
+        String url = setUrl(this.objectTitle);
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+
+    private String setUrl(TextView title) {
+        Resources res = getResources();
+        String toCompare= title.getText().toString();
+        String url=null;
+        switch(toCompare){
+            case "balon":
+                url=String.format(res.getString(R.string.balon_url));
+                break;
+            case "limon":
+                url=String.format(res.getString(R.string.limon_url));
+                break;
+            case "guitarra":
+                url=String.format(res.getString(R.string.guitarra_url));
+                break;
+            case "lampara":
+                url=String.format(res.getString(R.string.lampara_url));
+                break;
+            case "ordenador":
+                url=String.format(res.getString(R.string.ordenador_url));
+                break;
+            case "patata":
+                url=String.format(res.getString(R.string.patata_url));
+                break;
+            case "raqueta":
+                url=String.format(res.getString(R.string.raqueta_url));
+                break;
+            case "sol":
+                url=String.format(res.getString(R.string.sol_url));
+                break;
+            default:
+                Toast.makeText(getApplicationContext(),getText(R.string.error_image),Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return url;
     }
 
     public void nextObject() {
